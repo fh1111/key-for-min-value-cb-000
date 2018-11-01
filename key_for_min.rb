a3478ca877e32d187395ab1f7164375e9ce9dbad
Fer Hernandez <fh.1111@gmail.com>
2018-11-01 00:55:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  min = 0 
+  name_hash.collect do |item , number|
+
+    if (number < min)
+      min = number
+    end
+
+  end
+
+  puts min  
+    
+    
+
 end
+
+key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
